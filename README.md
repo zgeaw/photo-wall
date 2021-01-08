351ab3294a481b6f6a4c4a2a4b02d6b3b73ef1de
耿为刚 <32237384@qq.com>
2021-01-08 15:28:09 +0800

    updata

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # photoWall
 照片墙
+
+
+<img src="https://github.com/zgeaw/photo-wall/blob/master/dist/demo.png?raw=true"/>
--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
 
 module.exports = {
   runtimeCompiler: true,
-  publicPath: "/",
+  publicPath: "./",
   devServer: {
     port: 8081,
     overlay: {
